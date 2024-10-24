6e0d731df51a59b1ac9ddd96d8801425af4d229f
rodo08 <rodrigo.rosalesmoya@gmail.com>
2024-10-24 16:22:34 +0200

    Add scroll to top button and functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//top bar background after scroll
 const navbar = document.querySelector("nav");
 
 window.addEventListener("scroll", () => {
@@ -7,5 +7,16 @@ window.addEventListener("scroll", () => {
     navbar.classList.add("active");
   } else {
     navbar.classList.remove("active");
+  }
+});
+
+//scroll to top
+const scrollToTop = document.querySelector(".scrolltotop a");
+
+window.addEventListener("scroll", () => {
+  if (scrollY > 1000) {
+    scrollToTop.classList.add("active");
+  } else {
+    scrollToTop.classList.remove("active");
   }
 });
